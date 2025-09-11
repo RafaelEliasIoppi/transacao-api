bafb6adb625aca7d4f134c632f3f77140d750891
Rafael Elias Ioppi <141973836+RafaelEliasIoppi@users.noreply.github.com>
2025-09-11 17:21:10 +0000

    Rafael

--- src/main/java/desafio/transacao_api/controller/RootController.java
+++ src/main/java/desafio/transacao_api/controller/RootController.java
@@ -1,7 +1,9 @@
 package desafio.transacao_api.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
 public class RootController {
     @GetMapping("/")
     public String redirectToSwagger() {
